d109b160f07b33e61c01c91727cc10efa1bf3760
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2022-09-10 14:41:19 -0400

    Add overall header atem++.hpp

---
+++ src/atem++.hpp
@@ -0,0 +1,12 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2022 Dimitry Ishenko
+// Contact: dimitry (dot) ishenko (at) (gee) mail (dot) com
+//
+// Distributed under the GNU GPL license. See the LICENSE.md file for details.
+
+////////////////////////////////////////////////////////////////////////////////
+#include "atem++/aux_busses.hpp"
+#include "atem++/inputs.hpp"
+#include "atem++/mes.hpp"
+#include "atem++/switcher.hpp"
+#include "atem++/types.hpp"
